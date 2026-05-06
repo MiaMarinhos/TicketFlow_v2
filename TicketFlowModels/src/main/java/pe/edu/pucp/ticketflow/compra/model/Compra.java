ffb16835ee15c1546ee8965c8d15b175e342424c
MiaMarinhos <a20230411@pucp.edu.pe>
2026-05-06 17:07:28 -0500

    CompraModel

---
+++ TicketFlowModels/src/main/java/pe/edu/pucp/ticketflow/compra/model/Compra.java
@@ -0,0 +1,15 @@
+package pe.edu.pucp.ticketflow.compra.model;
+
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Compra {
+    private int idCompra;
+    private int entradasCompradas;
+    private LocalDate fechaCompra;
+    private LocalTime horaCompra;
+    private String metodoPago;
+    private String estado;
+
+}
