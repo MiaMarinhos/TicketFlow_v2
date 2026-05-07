cf7a33feb52a831b0ce24b18e9e26c69f21ac813
MiaMarinhos <a20230411@pucp.edu.pe>
2026-05-06 21:22:06 -0500

    DAOImpl

--- TicketFlowDAO/src/main/java/pe/edu/pucp/ticketflow/base/IBaseDAO.java
+++ TicketFlowDAO/src/main/java/pe/edu/pucp/ticketflow/base/IBaseDAO.java
@@ -3,9 +3,9 @@ package pe.edu.pucp.ticketflow.base;
 import java.util.List;
 
 public interface IBaseDAO <Tipo, ID>{
-    ID create(Tipo t);         // INSERT
-    Tipo read(ID id);           // SELECT por ID
-    boolean update(Tipo t, ID id);        // UPDATE
-    boolean delete(ID id);         // DELETE
-    List<Tipo> listAll();       // SELECT *
+    ID create(Tipo t);                  // INSERT
+    Tipo read(ID id);                   // SELECT por ID
+    boolean update(Tipo t, ID id);      // UPDATE
+    boolean delete(ID id);              // DELETE
+    List<Tipo> listAll();               // SELECT *
 }
---
+++ TicketFlowDAO/src/main/java/pe/edu/pucp/ticketflow/impl/CompraDAOImpl.java
@@ -0,0 +1,6 @@
+package pe.edu.pucp.ticketflow.impl;
+
+import pe.edu.pucp.ticketflow.ICompraDAO;
+
+public class CompraDAOImpl {
+}
