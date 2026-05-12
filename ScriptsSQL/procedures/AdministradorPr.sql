568a5b5c84cc84d73bce15711264fb1be1a7639d
josephandrader <joseph.andrader@pucp.edu.pe>
2026-05-11 19:58:55 -0500

    Cambio pequeño de Administrador

--- ScriptsSQL/procedures/AdministradorPr.sql
+++ ScriptsSQL/procedures/AdministradorPr.sql
@@ -1,10 +1,11 @@
------------------------------------
---ADMINISTRADOR
------------------------------------
---Create
-DELIMITER $$
+-- -----------------------------------------------------
+-- PROCEDURES PARA CAPA DE ADMINISTRADOR
+-- -----------------------------------------------------
 
-CREATE PROCEDURE sp_crear_administrador(
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS SP_INSERTAR_ADMINISTRADOR //
+CREATE PROCEDURE SP_INSERTAR_ADMINISTRADOR(
     IN p_idAdministrador INT,
     IN p_codigo VARCHAR(45),
     IN p_nombre VARCHAR(45),
@@ -14,7 +15,6 @@ CREATE PROCEDURE sp_crear_administrador(
     IN p_contrasena VARCHAR(45)
 )
 BEGIN
-
 INSERT INTO administrador(
     idAdministrador,
     codigo,
@@ -33,36 +33,26 @@ VALUES(
           p_dni,
           p_contrasena
       );
+END //
 
-END$$
-
-DELIMITER ;
-
---Read
-DELIMITER $$
-
-CREATE PROCEDURE USP_LEER_ADMINISTRADOR(
+DROP PROCEDURE IF EXISTS SP_LEER_ADMINISTRADOR //
+CREATE PROCEDURE SP_LEER_ADMINISTRADOR(
     IN p_idAdministrador INT
 )
 BEGIN
-SELECT
-    idAdministrador,
-    codigo,
-    nombre,
-    apellido_paterno,
-    apellido_materno,
-    dni,
-    contrasena
+SELECT idAdministrador,
+       codigo,
+       nombre,
+       apellido_paterno,
+       apellido_materno,
+       dni,
+       contrasena
 FROM administrador
 WHERE idAdministrador = p_idAdministrador;
-END$$
+END //
 
-DELIMITER ;
-
---Update
-DELIMITER $$
-
-CREATE PROCEDURE USP_ACTUALIZAR_ADMINISTRADOR(
+DROP PROCEDURE IF EXISTS SP_ACTUALIZAR_ADMINISTRADOR //
+CREATE PROCEDURE SP_ACTUALIZAR_ADMINISTRADOR(
     IN p_idAdministrador INT,
     IN p_codigo VARCHAR(45),
     IN p_nombre VARCHAR(45),
@@ -72,53 +62,37 @@ CREATE PROCEDURE USP_ACTUALIZAR_ADMINISTRADOR(
     IN p_contrasena VARCHAR(45)
 )
 BEGIN
-
 UPDATE administrador
-SET
-    codigo = p_codigo,
+SET codigo = p_codigo,
     nombre = p_nombre,
     apellido_paterno = p_apellido_paterno,
     apellido_materno = p_apellido_materno,
     dni = p_dni,
     contrasena = p_contrasena
 WHERE idAdministrador = p_idAdministrador;
+END //
 
-END$$
-
-DELIMITER ;
-
---Delete
-DELIMITER $$
-
-CREATE PROCEDURE USP_ELIMINAR_ADMINISTRADOR(
+DROP PROCEDURE IF EXISTS SP_ELIMINAR_ADMINISTRADOR //
+CREATE PROCEDURE SP_ELIMINAR_ADMINISTRADOR(
     IN p_idAdministrador INT
 )
 BEGIN
-
 DELETE FROM administrador
 WHERE idAdministrador = p_idAdministrador;
+END //
 
-END$$
-
-DELIMITER ;
-
-
---ListAll
-DELIMITER $$
-
-CREATE PROCEDURE USP_LISTAR_ADMINISTRADORES()
+DROP PROCEDURE IF EXISTS SP_LISTAR_ADMINISTRADORES //
+CREATE PROCEDURE SP_LISTAR_ADMINISTRADORES()
 BEGIN
-
-SELECT
-    idAdministrador,
-    codigo,
-    nombre,
-    apellido_paterno,
-    apellido_materno,
-    dni,
-    contrasena
-FROM administrador;
-
-END$$
+SELECT idAdministrador,
+       codigo,
+       nombre,
+       apellido_paterno,
+       apellido_materno,
+       dni,
+       contrasena
+FROM administrador
+ORDER BY idAdministrador;
+END //
 
 DELIMITER ;
